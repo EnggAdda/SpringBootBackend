b449e7132fed1e94467e450c2c928af837d8f3c2
Ravi Kumar Gupta <111415169+EnggAdda@users.noreply.github.com>
2023-02-06 01:10:59 +0530

    Spring Boot Backend

---
+++ src/main/java/com/example/springbootcrud/controller/ProductController.java
@@ -0,0 +1,49 @@
+package com.example.springbootcrud.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.springbootcrud.entity.Product;
+import com.example.springbootcrud.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public List<Product> findAll() {
+        return productService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Product> findById(@PathVariable Long id) {
+        return productService.findById(id);
+    }
+
+    @PostMapping
+    public Product save(@RequestBody Product product) {
+        return productService.save(product);
+    }
+
+    @PutMapping
+    public Product update(@RequestBody Product product) {
+        return productService.save(product);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable Long id) {
+        productService.deleteById(id);
+    }
+}
+
---
+++ src/main/java/com/example/springbootcrud/service/ProductService.java
@@ -0,0 +1,32 @@
+package com.example.springbootcrud.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.springbootcrud.entity.Product;
+import com.example.springbootcrud.repo.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> findById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public Product save(Product product) {
+        return productRepository.save(product);
+    }
+
+    public void deleteById(Long id) {
+        productRepository.deleteById(id);
+    }
+}
+
